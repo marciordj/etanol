b41df44fd70b8d8bfef7621e3d4ef9affa9f5dfe
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-12-27 00:00:20 -0300

    falta arrumar o calculo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,27 +1,52 @@
 import { useState } from 'react'
+import { Formik, Field, Form, FormikHelpers } from 'formik'
 import styles from './App.module.scss'
+
+interface Values {
+  gasoline: string;
+  etanol: string;
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
-  const calcFuel = () => {
-    document.getElementById('gasolina')
+  const calcFuel = (etanol: string, gasoline:string) => {
+    return (parseFloat(etanol) / parseFloat(gasoline)) * 100
   }
 
   return (
     <div className={styles.App}>
-      <div className={styles.fuels}>
+      <Formik initialValues={{
+        etanol: '',
+        gasoline: ''
+      }}
+      onSubmit={(
+        {etanol, gasoline}: Values,
+        {setSubmitting}: FormikHelpers<Values> 
+      ) => {
+        console.log('etanol: -->', etanol);
+        console.log('gasoline: -->', gasoline);
+        console.log(parseInt(etanol) / parseInt(gasoline))
+      }}
+      >
+
+      {/* <div className={styles.fuels}>
       <h3>Etanol</h3>
       <h3>X</h3>
       <h3>Gasolina</h3>
-      </div>
+      </div> */}
 
-      <div className={styles.inputFuelValue}>
-        <input type="text" id='gasolina' />
-        <input type="text" />
-      </div>
+      <Form className={styles.inputFuelValue}>
+        <label htmlFor="gasoline">Etanol</label>
+        <Field type="text" id='etanol' name='etanol' />
+        <label htmlFor="etanol">Gasolina</label>
+        <Field type='text' id='gasoline' name='gasoline' />
 
-      <button onClick={calcFuel} >Calcular</button>
+
+      <button type='submit'>Calcular</button>
+      </Form>
+      </Formik>
+
     </div>
   )
 }
